324686f053b987229f765edfdf858b306bbde821
ruangambeta5-cpu <ruangambeta5@gmail.com>
2026-05-09 12:07:28 -0300

    calculo do imc

---
+++ 03_python_entrada/entradas5.py
@@ -0,0 +1,15 @@
+# autor: Ruan Lucas
+# Projeto : entradas com nomes
+nome = input('Digite seu nome: ')
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+
+soma = valor1 + valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+subtracao = valor1 - valor2
+print(nome)
+print(f'A soma dos numeros é: {soma}')
+print(f'A multiplicação dos numeros é: {multiplicacao}')
+print(f'A divisão dos numeros é: {divisao}')
+print(f'A subtração dos numeros é: {subtracao}')
---
+++ 03_python_entrada/entradas6.py
@@ -0,0 +1,9 @@
+# autor: Ruan Lucas
+# Projeto: entrada pelo usuario
+
+peso = float(input('Digite o peso: '))
+altura = float(input('Digite a altura: '))
+imc = peso / (altura * altura)
+
+# exibir o resultado com variável
+print('seu IMC é: ', imc)
