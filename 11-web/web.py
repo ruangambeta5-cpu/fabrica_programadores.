4c095433c95145ec10aea0ba17e6db9098ca57cb
ruangambeta5-cpu <ruangambeta5@gmail.com>
2026-08-08 11:52:16 -0300

    Add files via upload

---
+++ 11-web/web.py
@@ -0,0 +1,12 @@
+# instalar o streamlit
+# pip install streamlit
+
+# importando a biblioteca
+import streamlit as st
+
+st.write("olá, mundo!!!")
+st.write("bem-vindo ao meu streamlit!")
+st.write("meu apelido é: mauricio farma aura")
+st.write("meu email é: mauriciomel_de_ouro_araujo@gmail.com")
+if st.button("Clique aqui"):
+    st.balloons()
